e428790123ceb1674b41bda4133bda5f19ba3527
vidit0210 <vidit0210@gmail.com>
2020-04-22 03:04:15 +0530

    C++

---
+++ Educative/C++DS/0.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  int x = 0;
+  x += 1;
+  cout<<"Running well";
+  return 0;
+
+}
---
+++ Educative/C++DS/1.cpp
@@ -0,0 +1,16 @@
+//
+// Created by vidit shah on 2020-04-22.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n = 10;
+    int sum = 0;
+    for (int i = 0; i < n; i++)
+    {
+        sum += 2;
+    }
+    return sum;
+}
